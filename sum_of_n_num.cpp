90b5adf544e8b25d18feb25b346653d3b2530206
tshirt04 <56727282+tshirt04@users.noreply.github.com>
2019-10-18 18:32:33 +0530

    another in c++

--- sum_of_n_num.cpp
+++ sum_of_n_num.cpp
@@ -1,17 +1,18 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
 	int n,i,j,sum=0;
-	scanf("%d",&n);
+ cin>>n;
 	int a[n];
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&a[i]);
+	cin>>a[i];
 	}
 	for(i=0;i<n;i++)
 	{
 		sum=sum+a[i];
 	}
-	printf("%d",sum);
+	cout<<sum;
 	return 0;
 }
