9665047602591af99941fd800b9f3be6c07d68f9
Afroz Quraishi <afroziiitian@gmail.com>
2020-04-19 23:33:11 +0530

    indentation added

--- Find_Minimum_of_n_number.cpp
+++ Find_Minimum_of_n_number.cpp
@@ -1,20 +1,21 @@
 #include<stdio.h>
-int main(){
-	int n,i,min;
-	scanf("%d",&n);
-	int a[n];
-	for(i=0;i<n;i++)
+int main()
+{
+    int n,i,min;
+    scanf("%d",&  n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>a[i])
 	{
-	scanf("%d",&a[i]);
+	    min=a[i];
 	}
-	min=a[0];
-	for(i=0;i<n;i++)
-	{
-		if(min>a[i])
-		{
-			min=a[i];
-		}
-	}
-	printf("%d",min);
-	return 0;
+    }
+    printf("%d",min);
+    return 0;
 }
