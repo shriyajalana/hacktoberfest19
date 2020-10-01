1499cd4bf0b99d112a54b8dee5eb0143b5f1c830
Amit Kumar <56012894+AmitAzadi@users.noreply.github.com>
2020-10-01 10:21:18 +0530

    sum_of_n_num.cpp

--- sum_of_n_num.cpp
+++ sum_of_n_num.cpp
@@ -2,16 +2,15 @@
 int main()
 {
 	int n,i,sum=0;
-	scanf("%d",&n);
+	
+	scanf("%d",&n);		// enter size of array
 	int a[n];
+	
 	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
+	scanf("%d",&a[i]);	// enter numbers 
+	
 	for(i=0;i<n;i++)
-	{
-		sum=sum+a[i];
-	}
-	printf("%d",sum);
-	return 0;
+	sum=sum+a[i];		// calculating sum
+	
+	printf("%d",sum);	// sum of a given number
 }
